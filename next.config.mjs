d61375b9ed1ad88d03b7f40b4b27dca7e09ea215
icodelakshay <lakshaysaxena2217@gmail.com>
2026-04-08 22:46:36 +0530

    feat: implement product storefront with filtering, grid layout, and quick-view functionality

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,32 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "bwkzjalnzktfzqycytbr.supabase.co", // Or whatever the Supabase instance is
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      }
+    ],
   },
 }
 
